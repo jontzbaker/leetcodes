94475078d865241e61e64541acede72166e74f1d
Jonathas Braga Baker <jontzbaker@hotmail.com>
2024-08-20 18:41:44 +0100

    Update two_sum.c

--- two_sum.c
+++ two_sum.c
@@ -1,68 +1,129 @@
-/* #define directives */
-#define TRUE    0x01
-#define FALSE   0x00
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
-/* Function definition */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+#define TRUE  1
+#define FALSE 0
+
+/* Reads the algarism at position, on linked list linked_number */
+unsigned int get_value(unsigned int position, struct ListNode* linked_number)
 {
-    _Bool   sum_not_found   = TRUE; /* Indicates whether the sum has NOT been found */
-    int*    ans             = NULL; /* Pointer used to return the answer */
-    int     current_sum     = 0;    /* Value of the current sum */    
-    int     index_0         = 0;    /* Index of the first number */
-    int     index_1         = 1;    /* Index of the second number */
-    int     status          = 0;    /* Indicates the status of the function. 0x00 means OK */
+    unsigned int value = 0;
+    struct ListNode* current_address = linked_number;
 
-    while (sum_not_found)
+    for (unsigned int i = 0; i++; i < position)
     {
-        /* First, calculate the current value of the sum */
-        current_sum = nums[index_0] + nums[index_1];
-
-        /* Then, check if the sum is correct */
-        if (current_sum == target)
+        if (current_address->next != NULL)
         {
-            sum_not_found = FALSE;
+            current_address = current_address->next;
         }
-        /* If it is not, shift the index by one */
         else
         {
-            index_1 ++;
-
-            /* Guard against index_0 and index_1 being the same */
-            if (index_0 == index_1)
-            {
-                index_1 ++;
-            }
-
-            /* Guard against index_1 going out of bounds */
-            if (index_1 > (numsSize - 1))
-            {
-                index_0 ++;
-                index_1 = 0;
-            }
-
-            /* Guard against index_0 going out of bounds */
-            if (index_0 > (numsSize - 1))
-            {
-                status = 6;
-            }
+            current_address = NULL;
+            break;
         }
     }
 
-    /* After a sum has been found, malloc the return array into the pointer */
-    if (status == 0)
+    if (current_address != NULL)
     {
-        ans = (int*) malloc(2 * sizeof(int));
-        *returnSize = 2;
-        ans[0] = index_0;
-        ans[1] = index_1;
+        value = current_address->val;
     }
-    /* NOTE: a single value returned means an error code */
     else
     {
-        ans = (int*) malloc(1 * sizeof(int));
-        *returnSize = 1;
-        ans[0] = status;
+        value = 11;
     }
 
-    return ans;
+    return value;
+}
+
+/* Writes the algarism at position, on linked list linked_number */
+void set_value(unsigned int position, struct ListNode* linked_number, unsigned int value)
+{
+    struct ListNode* current_address = linked_number;
+
+    for (unsigned int i = 0; i++; i < position)
+    {
+        current_address = current_address->next;
+    }
+
+    current_address->next = (struct ListNode*) malloc(sizeof(struct ListNode));
+
+    current_address->next->val = value;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+    struct ListNode* answer = NULL;
+    unsigned int depth = 0;
+    unsigned int v1 = 0;
+    unsigned int v2 = 0;
+    unsigned int v3 = 0;
+    _Bool keep_summing = TRUE;
+    _Bool pass_ten = FALSE;
+
+    while (keep_summing)
+    {
+        v1 = get_value(depth, l1);
+        v2 = get_value(depth, l2);
+
+        /* breaks loop if no more values */
+        if ((v1 > 9) && (v2 > 9))
+        {
+            keep_summing = FALSE;
+            
+            /* Corner case where ten was passed but no valid algarisms are found */
+            if (pass_ten)
+            {
+                set_value(depth, answer, 1);
+            }
+        }
+
+        /* keeps summing if there are at least one valid value */
+        else
+        {
+            v3 = 0;
+
+            /* If only one value is valid, then no sum is needed */
+            if (v1 > 9)
+            {
+                v3 = v2;
+            }
+            else if (v2 > 9)
+            {
+                v3 = v1;
+            }
+
+            /* If both values are valid, sum them */
+            else
+            {
+                v3 = v1 + v2;
+            }
+
+            /* Evaluate if pass_ten is TRUE and add one, accordingly */
+            if (pass_ten)
+            {
+                v3 += 1;
+            }
+
+            /* Pass ten forward if it is larger than a base-10 algarism */
+            if (v3 > 9)
+            {
+                pass_ten = TRUE;
+                v3 -= 10;
+            }
+        }
+
+        /* Allocate the new algarism if the inputs are not null */
+        if (keep_summing)
+        {
+            set_value(depth, answer, v3);
+            depth += 1;
+        }
+    }
+
+    return answer;
 }
